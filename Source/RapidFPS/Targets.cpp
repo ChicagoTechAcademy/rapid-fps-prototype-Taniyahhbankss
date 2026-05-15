525901091bb9c6b0457bc97726f956267e11a600
Taniyahhbankss <Pperry6@chicagotechacademy.org>
2026-05-15 11:27:20 -0500

    done vid 6

---
+++ Source/RapidFPS/Targets.cpp
@@ -0,0 +1,56 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Targets.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/SphereComponent.h"
+#include "FPSProjectile.h"
+
+// Sets default values
+ATargets::ATargets()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Collision = CreateDefaultSubobject<USphereComponent>(TEXT("Sphere Collision"));
+	SetRootComponent(Collision);
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Static Mesh"));
+	Mesh->SetupAttachment(RootComponent);
+
+	Collision->OnComponentHit.AddDynamic(this, &ATargets::OnHit);
+
+
+
+}
+
+// Called when the game starts or when spawned
+void ATargets::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ATargets::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+void ATargets::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	if (OtherActor && (OtherActor != this) && OtherComp)
+	{
+		// Handle logic here, e.g., print the name of the hit actor
+		UE_LOG(LogTemp, Warning, TEXT("Hit: %s"), OtherActor->GetName());
+
+		AFPSProjectile* projectile = Cast<AFPSProjectile>(OtherActor);
+
+		if (projectile)
+		{
+			Destroy();
+		}
+	}
+
+}
+
